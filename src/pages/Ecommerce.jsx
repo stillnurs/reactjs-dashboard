7d58b902c89fe9b6ee1fb68c70aa2a550e28d250
stillnurs <67423522+stillnurs@users.noreply.github.com>
2022-06-15 00:14:45 +0400

    added theming and dark mode

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,6 +6,9 @@ import { useStateContext } from "../contexts/ContextProvider";
 import { earningData, SparklineAreaData } from "../data/dummy";
 
 const Ecommerce = () => {
+
+  const { currentColor } = useStateContext();
+
   return <div className="mt-12">
     <div
       className="flex flex-wrap lg:flex-nowrap 
@@ -24,7 +27,7 @@ const Ecommerce = () => {
         <div className="mt-6">
           <Button
             color="white"
-            bgColor="blue"
+            bgColor={currentColor}
             text="Download"
             borderRadius="10px"
             size="md"
@@ -94,19 +97,19 @@ const Ecommerce = () => {
             </div>
             <div className="mt-5">
               <SparkLine
-                currentColor="blue"
+                currentColor={currentColor}
                 id="line-sparkline"
                 type="Line"
                 height="80px"
                 width="250px"
                 data={SparklineAreaData}
-                color="blue"
+                color={currentColor}
               />
             </div>
             <div className="mt-10">
               <Button
                 color="white"
-                bgColor="blue"
+                bgColor={currentColor}
                 text="Download Report"
                 borderRadius="10px"
               />
